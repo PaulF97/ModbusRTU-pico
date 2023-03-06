e108a8223fbfe6d8d6f507dc40fa732c861a8bf6
paul <paul.fisher@elsys-design.com>
2023-03-06 14:23:28 +0100

    add various performances tests

--- master.py
+++ master.py
@@ -25,22 +25,35 @@ print("test serial connexion")
 #  ------------------ VARIOUS TESTS ------------------
 
 #  ------------------ excecution time of write and read function ------------------
-startWrite = time.perf_counter()
-receive = master.write_registers(5, "bonjour".encode(), 0x01)
-endWrite = time.perf_counter()
-print("time write process", endWrite - startWrite)
-print("write transaction per seconds", 1/float((endWrite - startWrite)))
-startRead = time.perf_counter()
-receive = master.read_holding_registers(address=5, count=2, slave=0x01)
-endRead = time.perf_counter()
-print("time read process", endRead - startRead)
-print("read transaction per seconds", 1/float((endRead - startRead)))
-print(receive.__dict__)
+# startWrite = time.perf_counter()
+# receive = master.write_registers(5, "bonjour".encode(), 0x01)
+# endWrite = time.perf_counter()
+# print("time write process", endWrite - startWrite)
+# print("write transaction per seconds", 1/float((endWrite - startWrite)))
+# startRead = time.perf_counter()
+# receive = master.read_holding_registers(address=5, count=2, slave=0x01)
+# endRead = time.perf_counter()
+# print("time read process", endRead - startRead)
+# print("read transaction per seconds", 1/float((endRead - startRead)))
+# print(receive.__dict__)
 
 
-#  ------------------ serial ------------------
+#  ------------------ SERIAL ------------------
 # ser.write(b'bonjour')
 # ser.close()
 # receive = master.read_holding_registers(address=5, count=2, slave=0x01)
 
 
+# ------------------ SEND ZERO DATA BEFORE NORMAL FRAMES ------------------
+# send = master.write_registers(5,None, 0x01)
+# time.sleep(1)
+# while True:
+#     receive = master.write_registers(5, "bonjour".encode(), 0x01)
+#     time.sleep(1)
+
+
+# ------------------ SEND TO ILLEGAL ADDRESS BEFORE NORMAL FRAMES ------------------
+send = master.write_registers(5000,"test illegal address".encode(), 0x01)
+while True:
+    receive = master.write_registers(5, "bonjour".encode(), 0x01)
+    time.sleep(1)
